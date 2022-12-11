078a7575a0d49e534398b98d3d184b6c3a656dca
DanielToledo-99 <64055357+DanielToledo-99@users.noreply.github.com>
2022-12-10 21:20:06 -0500

    Add files via upload

---
+++ proyectofinal/Models/Entidades/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace proyectofinal.Models.Entidades
+{
+    public class Customer
+    {
+        public int IdCustomer { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        public int Phone { get; set; }
+        public string Email { get; set; }
+
+        public List<FacturaEntity> Factura { get; set; }
+    }
+}
---
+++ proyectofinal/Models/Entidades/FacturaEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace proyectofinal.Models.Entidades
+{
+    public class FacturaEntity
+    {
+        public int IdFactura { get; set; }
+
+        public int IdCustomer { get; set; }
+        public DateTime fechaCreacion  { get; set; }
+        public List<Product> Products { get; set; }
+        public bool estado { get; set; }
+
+    }
+}
